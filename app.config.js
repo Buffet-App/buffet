8c5ad72f14b926b68bf01270b35c60e2cdbd29fe
Daniel Inhee Lee <lee.danny112358@gmail.com>
2021-08-19 18:00:53 -0700

    firebase init

---
+++ app.config.js
@@ -0,0 +1,40 @@
+import "dotenv/config";
+
+export default {
+  expo: {
+    name: "BuffetApp",
+    slug: "BuffetApp",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./buffet-src/assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./buffet-src/assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF",
+      },
+    },
+    web: {
+      favicon: "./buffet-src/assets/favicon.png",
+    },
+    extra: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+    },
+  },
+};
