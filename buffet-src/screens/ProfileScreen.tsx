e50cee15d85596577fcf141c876e8ff04469e1c8
Daniel Inhee Lee <lee.danny112358@gmail.com>
2021-08-23 20:30:02 -0700

    fixed merge conflict remanants

--- buffet-src/screens/ProfileScreen.tsx
+++ buffet-src/screens/ProfileScreen.tsx
@@ -1,15 +1,11 @@
 import React from "react";
-<<<<<<< HEAD
-import { View, StyleSheet, Text, TouchableOpacity, ScrollView } from "react-native";
-=======
 import {
-  SafeAreaView,
+  ScrollView,
   View,
   StyleSheet,
   Text,
   TouchableOpacity,
 } from "react-native";
->>>>>>> added all bottom tab buttons + mapview + qr scanner + home page headers
 
 import Firebase from "../../config/firebase";
 import colors from "../config/colors";
@@ -26,52 +22,51 @@ const handleLogOut = async () => {
 
 const ProfileScreen = () => {
   return (
-<<<<<<< HEAD
     <View style={styles.container}>
       {/*<Text>Profile Screen!</Text>*/}
       <ScrollView>
-          <View style={styles.profileStat}>
-              <Text style={styles.captionTitle}>Name</Text>
-              <Text style={styles.captionText}>PLACEHOLDER</Text>
-          </View>
+        <View style={styles.profileStat}>
+          <Text style={styles.captionTitle}>Name</Text>
+          <Text style={styles.captionText}>PLACEHOLDER</Text>
+        </View>
 
-          <View style={styles.profileStat}>
-              <Text style={styles.captionTitle}>Email</Text>
-              <Text style={styles.captionText}>PLACEHOLDER</Text>
-          </View>
+        <View style={styles.profileStat}>
+          <Text style={styles.captionTitle}>Email</Text>
+          <Text style={styles.captionText}>PLACEHOLDER</Text>
+        </View>
 
-          <View style={styles.profileStat}>
-              <Text style={styles.captionTitle}>Phone</Text>
-              <Text style={styles.captionText}>PLACEHOLDER</Text>
-          </View>
+        <View style={styles.profileStat}>
+          <Text style={styles.captionTitle}>Phone</Text>
+          <Text style={styles.captionText}>PLACEHOLDER</Text>
+        </View>
 
-          <View style={styles.profileStat}>
-              <Text style={styles.captionTitle}>Subscribed Since</Text>
-              <Text style={styles.captionText}>PLACEHOLDER</Text>
-          </View>
+        <View style={styles.profileStat}>
+          <Text style={styles.captionTitle}>Subscribed Since</Text>
+          <Text style={styles.captionText}>PLACEHOLDER</Text>
+        </View>
 
-          <View style={styles.profileStat}>
-              <Text style={styles.captionTitle}>Subscription Status</Text>
-              <Text style={styles.captionText}>PLACEHOLDER</Text>
-          </View>
+        <View style={styles.profileStat}>
+          <Text style={styles.captionTitle}>Subscription Status</Text>
+          <Text style={styles.captionText}>PLACEHOLDER</Text>
+        </View>
 
-          <TouchableOpacity style={styles.signupButton} onPress={() => {console.log('want to edit profile')}}>
-              <Text style={styles.captionText}>Edit Profile</Text>
-          </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.signupButton}
+          onPress={() => {
+            console.log("want to edit profile");
+          }}
+        >
+          <Text style={styles.captionText}>Edit Profile</Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity style={[styles.signupButton, {marginBottom: 20}]} onPress={handleLogOut}>
-              <Text style={styles.captionText}>Log Out</Text>
-          </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.signupButton, { marginBottom: 20 }]}
+          onPress={handleLogOut}
+        >
+          <Text style={styles.captionText}>Log Out</Text>
+        </TouchableOpacity>
       </ScrollView>
     </View>
-=======
-    <SafeAreaView>
-      <Text>Profile Screen!</Text>
-      <TouchableOpacity style={styles.signupButton} onPress={handleLogOut}>
-        <Text>Log Out</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
->>>>>>> added all bottom tab buttons + mapview + qr scanner + home page headers
   );
 };
 
@@ -108,7 +103,7 @@ const styles = StyleSheet.create({
   },
   captionTitle: {
     color: colors.white,
-  }
+  },
 });
 
 export default ProfileScreen;
