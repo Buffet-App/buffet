ec7ddd81d20a1e60b452b4b3a70ed680a0003d3f
jchao537 <jchao537@github.com>
2021-08-24 21:11:36 -0700

    Restaurant Interface and Basic Profile Screen

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { AuthenticatedUserProvider } from "./buffet-src/navigation/AuthenticatedUserProvider";
 import RootNavigator from "./buffet-src/navigation/RootNavigator";
 
+import RestaurantProfileScreen from "./buffet-src/screens/RestaurantProfileScreen";
+
 export default function App() {
   return (
     <AuthenticatedUserProvider>
