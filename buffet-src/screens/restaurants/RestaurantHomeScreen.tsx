4e47bc940b27d60aa927bd02593a14ab8a7d920f
Daniel Inhee Lee <lee.danny112358@gmail.com>
2021-09-05 18:17:40 -0700

    create headers for restaurant home screen to link to other tabs

--- buffet-src/screens/restaurants/RestaurantHomeScreen.tsx
+++ buffet-src/screens/restaurants/RestaurantHomeScreen.tsx
@@ -3,16 +3,75 @@ import {
   Image,
   SafeAreaView,
   StyleSheet,
+  ScrollView,
+  View,
   Text,
   TouchableOpacity,
 } from "react-native";
 
-const RestaurantHomeScreen: React.FunctionComponent = () => {
+import colors from "../../config/colors";
+
+const RestaurantHomeScreen = ({ navigation }) => {
   return (
-    <SafeAreaView>
-      <Text>Restaurants Home Screen!</Text>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.section}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("Buffet");
+            }}
+          >
+            <Text style={styles.text__header}>Overview</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.section}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("AnalyticsStack");
+            }}
+          >
+            <Text style={styles.text__header}>Analytics</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.section}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("DealsStack");
+            }}
+          >
+            <Text style={styles.text__header}>Manage Deals</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.section}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("BankStack");
+            }}
+          >
+            <Text style={styles.text__header}>Revenue</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
+  text__header: {
+    fontSize: 24,
+    margin: 5,
+    fontWeight: "bold",
+    color: colors.primary,
+  },
+  section: {
+    padding: 10,
+  },
+});
 
 export default RestaurantHomeScreen;
