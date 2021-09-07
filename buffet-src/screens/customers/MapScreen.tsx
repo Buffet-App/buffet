98139dcbeaf23f3c6c19a9b735f0374f239e3adc
jchao537 <jchao537@github.com>
2021-09-07 11:52:09 -0700

    Marker setup

--- buffet-src/screens/customers/MapScreen.tsx
+++ buffet-src/screens/customers/MapScreen.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import MapView from "react-native-maps";
+import React, {useState} from "react";
+import MapView, { Marker } from "react-native-maps";
 import {
   FlatList,
   ScrollView,
@@ -12,10 +12,69 @@ import { globalStyles } from "../../config/globalStyles";
 import Card from "../../components/Card";
 import { restaurants } from "./HomeScreen";
 
+export interface IMarkerObject {
+  coordinate: {
+    latitude: number,
+    longitude: number,
+  },
+  title: string,
+  description: string,
+}
+
+const markers: IMarkerObject[] = [
+  {
+    coordinate: {
+      latitude: 34.072778,
+      longitude: -118.441944,
+    },
+    title: 'Royce Hall',
+    description: '340 Royce Drive'
+  },
+  {
+    coordinate: {
+      latitude: 34.070211,
+      longitude: -118.446775,
+    },
+    title: 'Pauley Pavilion',
+    description: '301 Westwood Plaza'
+  },
+  {
+    coordinate: {
+      latitude: 34.073582702728885,
+      longitude: -118.4522479327651,
+    },
+    title: 'Hedrick Hall',
+    description: '250 De Neve Dr'
+  },
+];
+
 const MapScreen = ({ navigation }) => {
+  const [region, setRegion] = useState({
+    latitude: 34.0689,
+    longitude: -118.4452,
+    latitudeDelta: 0.015,
+    longitudeDelta: 0.015
+  });
+
   return (
     <ScrollView>
-      <MapView style={styles.map} />
+      <MapView
+          style={styles.map}
+          region={region}
+          onRegionChangeComplete={region => setRegion(region)}
+      >
+        {markers.map((marker, index) => (
+            <Marker
+                key={index}
+                coordinate={{
+                  latitude: marker.coordinate.latitude,
+                  longitude: marker.coordinate.longitude,
+                }}
+                title={marker.title}
+                description={marker.description}
+            />
+        ))}
+      </MapView>
       <Text style={globalStyles.headerText}>restaurants</Text>
       <FlatList
         nestedScrollEnabled={true}
